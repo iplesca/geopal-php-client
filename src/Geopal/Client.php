ba5c899116b7ccef318d6f5de2cd6764d4ddd376
Sean OReilly <sean.oreilly@geopal-solutions.com>
2013-09-19 18:06:14 +0100

    updates to add in client class

--- src/Geopal/Client.php
+++ src/Geopal/Client.php
@@ -4,5 +4,23 @@ namespace Geoapl\Client;
 
 class Client
 {
+    /**
+     * @var int
+     */
+    private $employeeId;
 
+    /**
+     * @var string
+     */
+    private $privateKey;
+
+    /**
+     * @param int $employeeId
+     * @param string $privateKey
+     */
+    public function __construct($employeeId, $privateKey)
+    {
+        $this->employeeId = $employeeId;
+        $this->privateKey = $privateKey;
+    }
 }
---
+++ tests/ClientTest.php
@@ -0,0 +1,7 @@
+<?php
+namespace Geoapl\Tests;
+
+class ClientTest extends \PHPUnit_Framework_TestCase
+{
+
+}
